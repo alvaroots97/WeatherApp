9f2481388741f08075131af187eab51cd4405601
Alvaro Alva <7mesino.97@gmail.com>
2020-09-18 19:16:09 -0400

    Initial Commit

---
+++ WeatherApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  WeatherApp
+//
+//  Created by Alvaro Alva on 9/18/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ WeatherApp/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  WeatherApp
+//
+//  Created by Alvaro Alva on 9/18/20.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
